dadddff114b0a48c12a6a36f382c4194b3de6d2f
Sarah L <sarahl@bu.edu>
2025-09-26 14:27:29 -0400

    changes

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -5,7 +5,7 @@ from .views import  ProfileListView, RandomProfileView, ProfileDetailView
 
 urlpatterns=[
     path('',RandomProfileView.as_view(),name="random"),
-    path('show_all',ProfileListView.as_view(), name="show_all"),
+    path('show_all/',ProfileListView.as_view(), name="show_all"),
     path('profile/<int:pk>/',ProfileDetailView.as_view(), name='profile')
 
 ]
